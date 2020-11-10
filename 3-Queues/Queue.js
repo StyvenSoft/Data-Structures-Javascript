e882c1cac7e6a8078f1eaabaefe6e0f9f9a40415
Steveen <steveen.echeverri@gmail.com>
2020-11-10 16:16:58 -0500

    Avoiding Overflow

--- 3-Queues/Queue.js
+++ 3-Queues/Queue.js
@@ -1,35 +1,39 @@
 const LinkedList = require('./LinkedList');
 
 class Queue {
-    constructor(maxSize = Infinity) {
-        this.queue = new LinkedList();
-        this.maxSize = maxSize;
-        this.size = 0;
-    }
+  constructor(maxSize = Infinity) {
+    this.queue = new LinkedList();
+    this.maxSize = maxSize;
+    this.size = 0;
+  }
 
-    isEmpty() {
-        return this.size === 0;
-    }
+  isEmpty() {
+    return this.size === 0;
+  }
 
-    hasRoom() {
-        return this.size < this.maxSize;
-    }
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
 
-    enqueue(data) {
-        this.queue.addToTail(data);
-        this.size++;
-        console.log(`Added ${data}! Queue size is now ${this.size}.`);
+  enqueue(data) {
+    if (this.hasRoom()) {
+      this.queue.addToTail(data);
+      this.size++;
+      console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+    } else {
+      throw new Error("Queue is full!");
     }
-    dequeue() {
-        if (!this.isEmpty()) {
-          const data = this.queue.removeHead();
-          this.size--;
-          console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
-          return data;
-        } else {
-          throw new Error("Queue is empty!");
-        }
-      }
+  }
+  dequeue() {
+    if (!this.isEmpty()) {
+      const data = this.queue.removeHead();
+      this.size--;
+      console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+      return data;
+    } else {
+      throw new Error("Queue is empty!");
+    }
+  }
 }
 
 module.exports = Queue;
