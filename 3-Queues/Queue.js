f2615cf51d98cda20c63fba58ed44248e81a650a
Steveen <steveen.echeverri@gmail.com>
2020-11-09 22:58:46 -0500

    Conditions when enqueuing and dequeuing

--- 3-Queues/Queue.js
+++ 3-Queues/Queue.js
@@ -1,9 +1,18 @@
 const LinkedList = require('./LinkedList');
 
 class Queue {
-    constructor() {
+    constructor(maxSize = Infinity) {
         this.queue = new LinkedList();
+        this.maxSize = maxSize;
         this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    hasRoom() {
+        return this.size < this.maxSize;
     }
 
     enqueue(data) {
@@ -11,12 +20,31 @@ class Queue {
         this.size++;
         console.log(`Added ${data}! Queue size is now ${this.size}.`);
     }
+    dequeue() {
+        if (!this.isEmpty()) {
+          const data = this.queue.removeHead();
+          this.size--;
+          console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+          return data;
+        } else {
+          throw new Error("Queue is empty!");
+        }
+      }
 }
 
 module.exports = Queue;
 
 const restaurantOrder = new Queue();
-console.log(`restaurantOrder queue has ${restaurantOrder.size} orders.\n`);
 restaurantOrder.enqueue('apple pie');
 restaurantOrder.enqueue('roast chicken');
 restaurantOrder.enqueue('quinoa salad');
+console.log('\nFood preparing...\n')
+restaurantOrder.dequeue();
+restaurantOrder.dequeue();
+restaurantOrder.dequeue();
+console.log('All orders ready!')
+
+const boundedQueue = new Queue(3);
+boundedQueue.enqueue(1);
+boundedQueue.enqueue(2);
+boundedQueue.enqueue(3);
