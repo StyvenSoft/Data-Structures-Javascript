19fd679d65545aebe7402dd1f01fca6745e39703
Steveen <steveen.echeverri@gmail.com>
2020-11-13 10:59:19 -0500

    Adding and removing a Child

--- 6-Trees/TreeNode.js
+++ 6-Trees/TreeNode.js
@@ -3,6 +3,29 @@ class TreeNode {
         this.data = data;
         this.children = [];
     }
+
+    addChild(child) {
+        if (child instanceof TreeNode) {
+            this.children.push(child);
+        } else {
+            this.children.push(new TreeNode(child));
+        }
+    }
+
+    removeChild(childToRemove) {
+        const length = this.children.length;
+        this.children = this.children.filter(child => {
+            if (childToRemove instanceof TreeNode) {
+                return childToRemove !== child;
+            } else {
+                return child.data !== childToRemove;
+            }
+        });
+
+        if (length === this.children.length) {
+            this.children.forEach(child => child.removeChild(childToRemove));
+        }
+    }
 };
 
 module.exports = TreeNode;
--- 6-Trees/script.js
+++ 6-Trees/script.js
@@ -5,3 +5,29 @@ const tree = new TreeNode(1);
 
 // display your TreeNode class here
 console.log(tree);
+
+tree.addChild(15);
+console.log(tree);
+tree.addChild(new TreeNode(30));
+console.log(tree);
+
+// TreeNode { data: 1, children: [] }
+// TreeNode { data: 1, children: [ TreeNode { data: 15, children: [] } ] }
+// TreeNode {
+//   data: 1,
+//   children: [
+//     TreeNode { data: 15, children: [] },
+//     TreeNode { data: 30, children: [] }
+//   ]
+// }
+
+tree.addChild(15);
+const node = new TreeNode(30);
+tree.addChild(node);
+console.log(tree);
+
+tree.removeChild(15);
+console.log(tree);
+
+tree.removeChild(node);
+console.log(tree);
