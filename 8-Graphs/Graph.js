80367e1bfad10a69a4535dc853e24c2907291da6
Steveen <steveen.echeverri@gmail.com>
2020-11-15 17:32:41 -0500

    Weighted and directed graphs

--- 8-Graphs/Graph.js
+++ 8-Graphs/Graph.js
@@ -2,8 +2,10 @@ const Edge = require('./Edge.js');
 const Vertex = require('./Vertex.js');
 
 class Graph {
-    constructor() {
+    constructor(isWeighted = false, isDirected = false) {
         this.vertices = [];
+        this.isWeighted = isWeighted;
+        this.isDirected = isDirected;
     }
 
     addVertex(data) {
@@ -17,6 +19,30 @@ class Graph {
         this.vertices = this.vertices.filter(v => v !== vertex);
     }
 
+    addEdge(vertexOne, vertexTwo, weight) {
+        const edgeWeight = this.isWeighted ? weight : null;
+
+        if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+            vertexOne.addEdge(vertexTwo, edgeWeight);
+            if (!this.isDirected) {
+                vertexTwo.addEdge(vertexOne, edgeWeight);
+            }
+        } else {
+            throw new Error('Expected Vertex arguments.');
+        }
+    }
+
+    removeEdge(vertexOne, vertexTwo) {
+        if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+            vertexOne.removeEdge(vertexTwo);
+            if (!this.isDirected) {
+                vertexTwo.removeEdge(vertexOne);
+            }
+        } else {
+            throw new Error('Expected Vertex arguments.');
+        }
+    }
+
     print() {
         // const vertexList = this.vertices || [];
         // vertexList.forEach(vertex => vertex.print());
@@ -24,11 +50,16 @@ class Graph {
     }
 }
 
-const trainNetwork = new Graph();
+const trainNetwork = new Graph(false, true);
 const atlantaStation = trainNetwork.addVertex('Atlanta');
 const newYorkStation = trainNetwork.addVertex('New York');
 
-trainNetwork.removeVertex(atlantaStation);
+// trainNetwork.removeVertex(atlantaStation);
+trainNetwork.addEdge(atlantaStation, newYorkStation, 800);
+
+//trainNetwork.addEdge(atlantaStation, newYorkStation);
+
+trainNetwork.removeEdge(atlantaStation, newYorkStation);
 
 trainNetwork.print();
 
---
+++ 8-Graphs/trainNetwork.js
@@ -0,0 +1,31 @@
+const Graph = require('./Graph.js');
+
+const trainNetwork = new Graph(true, true);
+const laStation = trainNetwork.addVertex('Los Angeles');
+const sfStation = trainNetwork.addVertex('San Francisco');
+const nyStation = trainNetwork.addVertex('New York');
+const atlStation = trainNetwork.addVertex('Atlanta');
+const denStation = trainNetwork.addVertex('Denver');
+const calStation = trainNetwork.addVertex('Calgary');
+
+trainNetwork.addEdge(sfStation, laStation, 400);
+trainNetwork.addEdge(laStation, sfStation, 400);
+trainNetwork.addEdge(nyStation, denStation, 1800);
+trainNetwork.addEdge(denStation, nyStation, 1800);
+trainNetwork.addEdge(calStation, denStation, 1000);
+trainNetwork.addEdge(denStation, calStation, 1000);
+trainNetwork.addEdge(atlStation, laStation, 2100);
+trainNetwork.addEdge(laStation, atlStation, 2100);
+
+trainNetwork.removeEdge(nyStation, denStation);
+trainNetwork.removeEdge(calStation, denStation);
+trainNetwork.removeEdge(denStation, calStation);
+trainNetwork.removeVertex(calStation);
+
+trainNetwork.print();
+
+// Los Angeles --> San Francisco (400), Atlanta (2100)
+// San Francisco --> Los Angeles (400)
+// New York -->
+// Atlanta --> Los Angeles (2100)
+// Denver --> New York (1800)
